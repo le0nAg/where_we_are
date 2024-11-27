b42e9b01755f12d4a48bb6811118a9795fcf0a81
FiloFav <filofav04@gmail.com>
2024-11-27 11:15:12 +0100

    Creata la base del frontend del gestionale, in particolare selezione di un POI su mappa e visualizzazione della mappa

---
+++ src/webapp/frontend/management/src/pages/MapPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MapComponent from "../components/MapComponent";
+
+const MapPage = () => {
+  return (
+    <div className="page">
+      <h1>Mappa</h1>
+      <MapComponent />
+    </div>
+  );
+};
+
+export default MapPage;
---
+++ src/webapp/frontend/management/src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="page">
+      <h1>Impostazioni</h1>
+      <p>Modifica le impostazioni del tuo gestionale.</p>
+    </div>
+  );
+};
+
+export default Settings;
