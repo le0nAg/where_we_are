7dc2d71da8122e4d6cbf4d52d360ecbccdca7e3f
le0nAg <leonardoagostini123@email.com>
2025-01-09 15:27:37 +0100

    removed ts

--- src/webapp/server/src/routes/authnRoutes.ts
+++
@@ -1,9 +0,0 @@
-// File: src/routes/authRoutes.ts
-import express from 'express';
-import { login } from '../controllers/authnController';
-
-const router = express.Router();
-
-router.post('/login', login);
-
-export default router;
