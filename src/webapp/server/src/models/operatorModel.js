8fc41a2ef7c62885dd668b63d38cc2e89b7056ca
le0nAg <leonardoagostini123@email.com>
2025-01-15 15:04:40 +0100

    moving hashing from bcrypt to argon2 according to OWASP directives

--- src/webapp/server/src/models/operatorModel.js
+++ src/webapp/server/src/models/operatorModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const argon2 = require('argon2');
 
 const operatorSchema = new mongoose.Schema({
   email: {
@@ -22,7 +22,7 @@ const operatorSchema = new mongoose.Schema({
 });
 
 operatorSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = await argon2.hash(this.password, 12);
 });
 
 module.exports = mongoose.model("Operator", operatorSchema);
